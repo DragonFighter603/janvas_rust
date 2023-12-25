27b48ca75dc579a5aadda1f274e67155ca6dcb36
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-12-25 11:05:03 +0100

    events

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,37 @@ macro_rules! log {
 }
 
 #[macro_export]
-macro_rules! game {
-    (game: ident) => {
-        game
+macro_rules! create_game {
+    ($game: ident) => {
+        fn create() -> $game {
+
+        }
     };
 }
 
-pub trait Game {
+#[macro_export]
+macro_rules! js_field {
+    ($object: expr => $field: ident) => {
+        js_sys::Reflect::get($object, &JsValue::from_str( stringify!($field) )).unwrap();
+    };
+}
+
+pub trait JsGame {
     fn create() -> Self;
     fn init(&mut self);
     fn update(&mut self);
     fn render(&mut self);
-    fn event(&mut self, event: JsValue);
+    fn event(&mut self, event: &JsValue);
+}
+
+pub trait InputHandler {
+    fn handle(&mut self, event: &JsValue) -> bool {
+        match js_field!(event => type).as_string().unwrap().as_str() {
+            event_id => self.default(event_id, event)
+        }
+    }
+
+    fn default(&mut self, event_id: &str, event: &JsValue) -> bool {
+        false
+    }
 }
