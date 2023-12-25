b79e175048c44abfe708b296ebd37733618d38c6
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-12-25 11:44:38 +0100

    s

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use web_sys::wasm_bindgen::JsValue;
+use web_sys::{wasm_bindgen::JsValue, CanvasRenderingContext2d};
+
+pub use console_error_panic_hook;
+pub use wasm_bindgen;
 
 #[macro_export]
 macro_rules! log {
@@ -8,16 +11,11 @@ macro_rules! log {
 }
 
 #[macro_export]
-macro_rules! create_game {
-    ($game: ident) => {
-        fn create() -> $game {
-
-        }
-    };
-}
-
-#[macro_export]
 macro_rules! js_field {
+    ($object: expr => $field: ident $(=> $fields: ident)+) => {
+        js_field!(js_sys::Reflect::get($object, &JsValue::from_str( stringify!($field) )).unwrap() $(=> $fields)+);
+    };
+
     ($object: expr => $field: ident) => {
         js_sys::Reflect::get($object, &JsValue::from_str( stringify!($field) )).unwrap();
     };
@@ -25,20 +23,68 @@ macro_rules! js_field {
 
 pub trait JsGame {
     fn create() -> Self;
-    fn init(&mut self);
-    fn update(&mut self);
-    fn render(&mut self);
-    fn event(&mut self, event: &JsValue);
+    fn init(&mut self, context: CanvasContext);
+    fn update(&mut self, context: CanvasContext);
+    fn render(&mut self, canvas: &CanvasRenderingContext2d, context: CanvasContext);
+    fn event(&mut self, event: &JsValue, context: CanvasContext) where Self: JsInputHandler {
+        self.handle(event, context);
+    }
 }
 
 pub trait JsInputHandler {
-    fn handle(&mut self, event: &JsValue) -> bool {
+    fn handle(&mut self, event: &JsValue, context: CanvasContext) -> bool {
         match js_field!(event => type).as_string().unwrap().as_str() {
-            event_id => self.default(event_id, event)
+            event_id => self.default(event_id, event, context)
         }
     }
 
-    fn default(&mut self, event_id: &str, event: &JsValue) -> bool {
+    fn default(&mut self, event_id: &str, event: &JsValue, context: CanvasContext) -> bool {
         false
     }
+}
+
+pub struct CanvasContext {
+    pub width: u32,
+    pub height: u32,
+    pub time: f64,
+    pub time_delta: f64
+}
+
+#[macro_export]
+macro_rules! create_game {
+    ($game: ident) => {
+        use $crate::wasm_bindgen;
+        use $crate::wasm_bindgen::prelude::wasm_bindgen;
+
+        #[wasm_bindgen]
+        pub fn create() -> $game {
+            $game::create()
+        }
+
+        #[wasm_bindgen]
+        pub fn init(game: &mut $game, width: u32, height: u32, time: f64) {
+            game.init($crate::CanvasContext { width, height, time, time_delta: 1.0 } );
+        }
+
+        #[wasm_bindgen]
+        pub fn render(game: &mut $game, canvas: &web_sys::CanvasRenderingContext2d, width: u32, height: u32, time: f64, time_delta: f64) {
+            game.render(canvas, $crate::CanvasContext { width, height, time, time_delta });
+        }
+
+        #[wasm_bindgen]
+        pub fn update(game: &mut $game, width: u32, height: u32, time: f64, time_delta: f64) {
+            game.update($crate::CanvasContext { width, height, time, time_delta: f64::min(time_delta, 100.0) });
+        }
+
+        #[wasm_bindgen]
+        pub fn event(game: &mut $game, event: &JsValue, width: u32, height: u32, time: f64, time_delta: f64) {
+            game.event(event, $crate::CanvasContext { width, height, time, time_delta: f64::min(time_delta, 100.0) });
+        }
+
+        pub use $crate::console_error_panic_hook::set_once as set_panic_hook;
+        #[wasm_bindgen]
+        pub fn init_panic(){
+            std::panic::set_hook(Box::new($crate::console_error_panic_hook::hook));
+        }
+    };
 }
